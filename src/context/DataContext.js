0c6c387c839712aaf58436a0ab285bde302c67a1
jayaramjayakrishnan <jayaramjayakrishnan@gmail.com>
2024-08-29 11:18:36 +0530

    configuration changes

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const DataContext = createContext();
 
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
 export const DataProvider = ({ children }) => {
   const [userGrowthData, setUserGrowthData] = useState([]);
   const [revenueData, setRevenueData] = useState([]);
@@ -15,26 +17,26 @@ export const DataProvider = ({ children }) => {
   const [loadingStreams, setLoadingStreams] = useState(true);
 
   const fetchUserGrowthData = async () => {
-    const response = await axios.get('http://localhost:3000/users');
+    const response = await axios.get(`${apiUrl}/users`);
     console.log(response.data);
     setUserGrowthData(response.data);
     setLoadingUserGrowth(false);
   };
 
   const fetchRevenueData = async () => {
-    const response = await axios.get('http://localhost:3000/revenue');
+    const response = await axios.get(`${apiUrl}/revenue`);
     setRevenueData(response.data);
     setLoadingRevenue(false);
   };
 
   const fetchTopSongsData = async () => {
-    const response = await axios.get('http://localhost:3000/topSongs');
+    const response = await axios.get(`${apiUrl}/topSongs`);
     setTopSongsData(response.data);
     setLoadingTopSongs(false);
   };
 
   const fetchStreamsData = async () => {
-    const response = await axios.get('http://localhost:3000/streams');
+    const response = await axios.get(`${apiUrl}/streams`);
     setStreamsData(response.data);
     setLoadingStreams(false);
   };
