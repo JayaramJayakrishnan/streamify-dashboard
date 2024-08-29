cf69baa74c13d77bbb91d42f42c0ebf997e4cea0
jayaramjayakrishnan <jayaramjayakrishnan@gmail.com>
2024-08-29 23:16:50 +0530

    fix

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -1,21 +1,44 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
 const path = require('path');
-const router = jsonServer.router(path.join(__dirname, '../../db.json'));
+const fs = require('fs');
+
+// Read the JSON file
+const dbPath = path.join(__dirname, '../../db.json');
+const db = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+
+// Create a new JSON Server instance
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
-
 exports.handler = async (event, context) => {
-  const result = await new Promise((resolve, reject) => {
-    server.handle(event, {}, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
+  const server = jsonServer.create();
+
+  // Apply middlewares
+  server.use(middlewares);
+  server.use(router);
+
+  return new Promise((resolve, reject) => {
+    // Create a mock request and response
+    const req = { ...event };
+    const res = {
+      statusCode: 200,
+      body: '',
+      headers: {},
+      setHeader: function (name, value) {
+        this.headers[name] = value;
+      },
+      end: function (body) {
+        this.body = body;
       }
+    };
+
+    // Handle the request
+    server(req, res => {
+      resolve({
+        statusCode: res.statusCode,
+        body: res.body,
+        headers: res.headers,
+      });
     });
   });
-  return result;
 };
