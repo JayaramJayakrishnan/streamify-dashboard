69ad3925aa4ec995fac377bd0f6df77eae582da8
jayaramjayakrishnan <jayaramjayakrishnan@gmail.com>
2024-08-29 23:38:13 +0530

    fix - file path

--- netlify/functions/top-songs.js
+++ netlify/functions/top-songs.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event, context) => {
-  const dbPath = path.join(__dirname, '../../public/db.json');
+  const dbPath = path.join(__dirname, '../../db.json');
   const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
 
   return {
